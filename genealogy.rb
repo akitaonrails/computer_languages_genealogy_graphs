e842203c5c2e9127093a0ffe84901c2282e03dc1
AkitaOnRails <fabioakita@gmail.com>
2015-06-19 13:44:35 -0300

    Fixing the year grouping Removing a cyclic reference between Miranda and Clean Separating the graphviz string formatting from the array of languages to avoid trying to use the list after it’s already transformed to a string Making the recent_languages, ancestors, main_ranks a dynamic construct instead of statically initializing them

--- genealogy.rb
+++ genealogy.rb
@@ -2,9 +2,9 @@ require 'erb'
 require 'pry'
 
 class Timeline
-  attr_accessor :languages, :recent_languages, :main_ranks, :main_ranks_groups, :ancestors
-  
-  def initialize
+  attr_accessor :languages, :recent_languages
+
+  def initialize(filter = nil)
     self.languages = {
       "Speedcode" => { year: 1953, author: "John Backus", wikipedia: "https://en.wikipedia.org/wiki/Speedcoding",
         pioneer: [ "interpreted"],
@@ -12,14 +12,14 @@ class Timeline
         influenced: [ "FORTRAN", "ALGOL 58", "C", "PL/I", "MUMPS" ] },
       "FORTRAN" => { year: 1957, author: "John Backus", wikipedia: "https://en.wikipedia.org/wiki/Fortran",
         pioneer: [ "optimizing compiler" ],
-        versions: [ ["FORTRAN II", 1958], ["FORTRAN IV", 1961], ["FORTRAN 66", 1966], ["FORTRAN 77", 1977], ["FORTRAN 90", 1992], 
+        versions: [ ["FORTRAN II", 1958], ["FORTRAN IV", 1961], ["FORTRAN 66", 1966], ["FORTRAN 77", 1977], ["FORTRAN 90", 1992],
                     ["FORTRAN 95", 1997], ["FORTRAN 2003", 2004], ["FORTRAN 2008", 2010], ["FORTRAN 2015", 2015] ],
         influenced: [ "ALGOL 58", "C", "PL/I", "MUMPS", "Julia" ] },
       "ALGOL 58" => { year: 1958, author: "ETH Zürich commitee", wikipedia: "https://en.wikipedia.org/wiki/ALGOL",
         pioneer: ["context-free grammar", "structured programming"],
         versions: [ ["ALGOL 60", 1960], ["ALGOL W", 1966], ["ALGOL 68", 1973] ],
         influenced: [ "C", "CPL", "Pascal", "ADA" ] },
-      "ALGOL 60" => { noshow: true, 
+      "ALGOL 60" => { noshow: true,
         versions: [],
         influenced: [ "Simula", "BCPL", "CLU" ] },
       "ALGOL 68" => { noshow: true, wikipedia: "https://en.wikipedia.org/wiki/ALGOL_68",
@@ -70,7 +70,7 @@ class Timeline
         influenced: [] },
       "Prolog" => { year: 1972, author: "Alain Colmerauer", wikipedia: "https://en.wikipedia.org/wiki/Prolog",
         pioneer: [ "logic programming", "theorem proving", "natural language processing", "relations, represented as facts and rules", "running a query over these relations" ],
-        versions: [], 
+        versions: [],
         influenced: [ "Erlang" ] },
       "CLU" => { year: 1974, author: "Barbara Liskov", wikipedia: "https://en.wikipedia.org/wiki/CLU_(programming_language)",
         pioneer: [ "early object-oriented programming", "abstract data types", "call-by-sharing", "iterators", "multiple return values (a form of parallel assignment)", "type-safe parameterized types", "type-safe variant types", "classes with constructors and methods, without inheritance" ],
@@ -84,7 +84,7 @@ class Timeline
         pioneer: ["express operations using English-like statements", "distinctly separate the description of data from the operations on it"],
         versions: [],
         influenced: ["COBOL"] },
-      "COBOL" => { year: 1959, author: " Conference on Data Systems Languages (CODASYL)", wikipedia: "https://en.wikipedia.org/wiki/COBOL", 
+      "COBOL" => { year: 1959, author: " Conference on Data Systems Languages (CODASYL)", wikipedia: "https://en.wikipedia.org/wiki/COBOL",
         pioneer: ["designed for business use"],
         versions: [ ["COBOL 60", 1960], ["ANSI COBOL", 1974], ["COBOL 2002", 2002], ["COBOL 2014", 2014] ],
         influenced: [] },
@@ -152,7 +152,7 @@ class Timeline
       "Clean" => { year: 1987, author: "Software Technology Research Group of Radboud University Nijmegen", wikipedia: "https://en.wikipedia.org/wiki/Clean_(programming_language)",
         pioneer: ["general-purpose purely functional computer programming language"],
         versions: [],
-        influenced: [ "Miranda", "Haskell"] },
+        influenced: [ "Haskell"] },
       "Caché" => { year: 1996, author: "InterSystems", wikipedia: "https://en.wikipedia.org/wiki/Cach%C3%A9_ObjectScript",
         versions: [],
         influenced: [] },
@@ -226,6 +226,7 @@ class Timeline
         versions: [ ["GNU Objective-C", 1992], ["Objective-C 2.0", 2006] ],
         influenced: [ "Groovy", "Java", "Swift" ] },
       "Swift" => { year: 2014, author: "Chris Lattner/Apple", wikipedia: "https://en.wikipedia.org/wiki/Swift_(programming_language)",
+        highlight: "orange",
         versions: [ ["Swift 1.2", 2015] ],
         influenced: [] },
       "Erlang" => { year: 1986, author: "Joe Armstrong, Robert Virding and Mike Williams", wikipedia: "https://en.wikipedia.org/wiki/Erlang_(programming_language)",
@@ -236,9 +237,11 @@ class Timeline
         versions: [ ["Elixir 1.0", 2014]],
         influenced: [] },
       "Go" => { year: 2007, author: "Robert Griesemer, Rob Pike, Ken Thompson/Google", wikipedia: "https://en.wikipedia.org/wiki/Go_(programming_language)",
+        highlight: "black",
         versions: [ ["Go 1.0", 2012], ["Go 1.4", 2015] ],
         influenced: [] },
       "Javascript" => { year: 1995, author: "Brendan Eich", wikipedia: "https://en.wikipedia.org/wiki/JavaScript",
+        highlight: "grey",
         versions: [ ["Javascript ES1", 1997], ["Javascript ES3", 1999], ["Javascript ES5.1", 2011], ["Javascript ES6", 2014] ],
         influenced: [] },
       "Rust" => { year: 2011, author: "Graydon Hoare, then Rust Project Developers", wikipedia: "https://en.wikipedia.org/wiki/Rust_(programming_language)",
@@ -257,6 +260,7 @@ class Timeline
         versions: [],
         influenced: [] },
       "Java" => { year: 1995, author: "James Gosling/Sun Microsystems", wikipedia: "https://en.wikipedia.org/wiki/Java_(programming_language)",
+        highlight: "purple",
         pioneer: ["general-purpose computer programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible"],
         versions: [ ["J2SE 5", 2005]],
         influenced: [ "ADA 2005", "C#", "Clojure", "D", "Javascript", "Scala", "PHP/FI", "Groovy", "Hack" ] },
@@ -265,6 +269,7 @@ class Timeline
         versions: [ ["Perl 5", 1993], ["Perl 5.6", 2000], ["Perl 5.8", 2002], ["Perl 5.10", 2007], ["Perl 5.20", 2014]],
         influenced: [ "Groovy", "Python", "PHP/FI", "Javascript", "Julia" ] },
       "Python" => { year: 1991, author: "Guido van Rossum", wikipedia: "https://en.wikipedia.org/wiki/Python_(programming_language)",
+        highlight: "yellow",
         pioneer: ["intended to be a highly readable language"],
         versions: [ ["Python 2", 2000], ["Python 3", 2008]],
         influenced: [ "D", "F#", "Go", "Groovy", "Javascript", "Julia", "Ruby", "Swift" ] },
@@ -272,6 +277,7 @@ class Timeline
         versions: [],
         influenced: [ "Groovy" ] },
       "PHP/FI" => { year: 1995, author: "Rasmus Lerdorf/The PHP Group", wikipedia: "https://en.wikipedia.org/wiki/PHP",
+        highlight: "blue",
         pioneer: ["server-side scripting language"],
         versions: [ ["PHP 3", 1997], ["PHP 4", 2000], ["PHP 5", 2004], ["PHP 5.6.10", 2015]],
         influenced: [] },
@@ -282,6 +288,7 @@ class Timeline
         versions: [],
         influenced: [] },
       "Ruby" => { year: 1996, author: "Yukihiro Matsumoto", wikipedia: "https://en.wikipedia.org/wiki/Ruby_(programming_language)",
+        highlight: "red",
         pioneer: ["dynamic, reflective, object-oriented, general-purpose programming language"],
         versions: [ ["Ruby 1.2", 1998], ["Ruby 1.4", 1999], ["Ruby 1.6", 2000], ["Ruby 1.8", 2003], ["Ruby 1.9", 2007], ["Ruby 2.1", 2013], ["Ruby 2.2", 2014]],
         influenced: [ "Groovy", "Clojure", "Elixir", "Julia", "Swift" ] },
@@ -289,96 +296,129 @@ class Timeline
         versions: [ ["Groovy 2.0", 2012], ["Groovy 2.4.3", 2015] ],
         influenced: [ ] }
       }
-    
-      self.recent_languages = ["C# 5", "C++ 1y", "Swift", "C11", "Perl 5.20", "ADA 2012", "Delphi XE4", "COBOL 2014", "FORTRAN 2015", "Rust 1.0", "Elixir 1.0", "Erlang 17.5", "Javascript ES6", "Go 1.0", "Self 4.5", "Julia", "Haskell 2010", "F# 3.1.1"]
-      
-      self.ancestors = ["Speedcode", "FORTRAN", "ALGOL 58", "COBOL", "LISP"]
-    
-      self.main_ranks = %w(1955 1960 1965 1970 1975 1980 1985 1990 1995 2000 2005 2010 2015)
-    
-      self.main_ranks_groups = [
-        { year: 1955, languages: ["Speedcode", "FORTRAN", "ALGOL 58", "FLOW-MATIC", "COBOL", "Lisp", "FORTRAN II"] },
-        { year: 1960, languages: ["CPL", "PL/I", "COBOL 60", "ALGOL 60", "SNOBOL", "APL", "FORTRAN IV"] },
-        { year: 1965, languages: ["Simula", "Simula 67", "BCPL", "B", "ALGOL W", "FORTRAN 66", "ISWIM", "MUMPS"] },
-        { year: 1970, languages: ["Modula", "Smalltalk", "C", "Pascal", "ANSI COBOL", "ALGOL 68", "Planner", "Prolog", "CLU", "Forth", "ML", "Hope", "Mesa"] },
-        { year: 1975, languages: ["C with Classes", "MODULA-2", "AWK", "FORTRAN 77", "Scheme"] },
-        { year: 1980, languages: ["C++", "Objective-C", "ADA", "ADA 83", "Turbo Pascal", "PIM 2", "ABC", "MODULA-3", "Common Lisp", "occam"] },
-        { year: 1985, languages: ["Simula 87", "Perl", "Eiffel", "ANSI ADA", "PIM 3", "Object Pascal", "PIM 4",  "Oberon", "Erlang", "Self", "Caml", "Miranda", "Clean"] },
-        { year: 1990, languages: ["Lua", "Python", "GNU Objective-C", "ANSI C", "Standard ML", "Perl 5", "Pike", "FORTRAN 90", "Dylan", "Haskell"] },
-        { year: 1995, languages: ["Java", "C99", "ADA 95", "Delphi", "Delphi 5", "ISO MODULA-2", "FORTRAN 95",  "Scheme R5RS", "Javascript", "Javascript ES1", "Javascript ES3", "REBOL", "REBOL 2", "OCaml",  "Haskell 98", "Caché", "PHP/FI", "PHP 3", "Ruby", "Ruby 1.2", "Ruby 1.4"] },
-        { year: 2000, languages: ["Python 2", "Scala", "C#", "D", "Perl 5.6", "Perl 5.8", "COBOL 2002", "Delphi 7", "FORTRAN 2003", "REBOL/Command", "REBOL/View", "REBOL/SDK", "PHP 4", "PHP 5", "Ruby 1.6", "Ruby 1.8", "Factor"] },
-        { year: 2005, languages: ["Python 3", "J2SE 5", "C# 2", "C# 3", "D2", "Perl 5.10", "Scheme R7RS", "Go", "Rust", "REBOL 3", "Clojure", "Haskell Prime", "F#", "Ruby 1.9", "Groovy"] },
-        { year: 2010, languages: ["C# 4", "C++ 0x", "C11", "ADA 2012", "Delphi XE", "Javascript ES5.1", "F# 2.0", "C# 5", "C++ 1y", "Swift", "C11", "Perl 5.20", "ADA 2012", "Delphi XE4", "COBOL 2014", "Elixir", "Elixir 1.0", "Javascript ES6", "Go 1.0", "Self 4.5", "Julia", "Haskell 2010", "F# 3.1.1", "Hack", "PHP 5.6.10" , "Ruby 2.1", "Ruby 2.2"] },
-        { year: 2015, languages: ["Rust 1.0", "FORTRAN 2015", "Erlang 17.5"] }
-      ]
+
+      self.recent_languages = ["C# 5", "C++ 1y", "Swift 1.2", "C11", "Perl 5.20", "ADA 2012", "Delphi XE4", "COBOL 2014", "FORTRAN 2015", "Rust 1.0", "Elixir 1.0", "Erlang 17.5", "Javascript ES6", "Go 1.0", "Self 4.5", "Julia", "Haskell 2010", "F# 3.1.1"]
+      #self.ancestors = ["Speedcode", "ALGOL 58", "FLOW-MATIC", "LISP"]
+
+      filter_ancestors(filter)
   end
-  
+
   def escape_list(list, separator = "; ")
     list.map { |word| "\"#{word}\"" }.join(separator)
   end
-  
+
   def filter(main_list, list)
     list.select { |word| main_list.include?(word) }
   end
-  
-  def formatted_main_rank_groups(list)
-    main_ranks_groups.map do |group| 
-      { 
-        year: group[:year], 
-        languages: escape_list(filter(list, group[:languages]))
+
+  def languages_filtered
+    @languages_filtered ||= languages.reject { |key| languages[key][:noshow] }
+  end
+
+  def recent_versions(year = 2014)
+    languages_filtered.keys.inject([]) do |acc, key|
+      acc += languages_filtered[key][:versions].select do |version|
+        version.last > year
+      end.map(&:first)
+    end
+  end
+
+  def all_languages
+    languages_filtered.
+      inject([]) do |acc, (key, value)|
+        acc += value[:versions].map(&:first) unless value[:versions].empty?
+        acc += value[:influenced]
+      end
+  end
+
+  def ancestors
+    languages_filtered.keys - all_languages
+  end
+
+  def grouped_by_year(versions = true)
+    languages_filtered.keys.inject([]) do |acc, key|
+      acc += [[key, languages_filtered[key][:year]]] +
+        ( versions ? languages_filtered[key][:versions] : [] )
+    end.group_by { |a| a.last }
+  end
+
+  def formatted_main_rank_groups(versions = true)
+    versions = grouped_by_year(versions)
+    groups = (1955..2015).step(5).map do |year|
+      {
+          year: year,
+          languages: versions.keys.
+            select     { |y| y >= (year - 5) && y < year }.
+            inject([]) { |acc, y| acc += versions[y].map(&:first) }.flatten
       }
     end
+    groups.each { |g| g[:formatted_languages] = escape_list(g[:languages])}
+    groups
   end
 
   def valid_group(list, accumulator)
     [list,
-      formatted_main_rank_groups(list),
       recent_languages.select { |e| accumulator.include?(e) },
       ancestors.select { |e| accumulator.include?(e) } ]
   end
 
   def valid_versions(separator = "; ")
-    keys = languages.keys.select { |key| !languages[key][:noshow] && !languages[key][:versions].empty? }
-    list = keys.inject({}) { |list, key| list.merge(key => languages[key]) }
+    list = languages_filtered.reject { |key| languages_filtered[key][:versions].empty? }
     accumulator = []
     list.each do |key, value|
       accumulator += value[:versions].map(&:first)
-      value[:versions] = escape_list(value[:versions].map(&:first), separator)
+      value[:formatted_versions] = escape_list(value[:versions].map(&:first), separator)
     end
     valid_group(list, accumulator)
   end
-  
+
   def valid_influenced(separator = "; ")
-    keys = languages.keys.select { |key| !languages[key][:noshow] && !languages[key][:influenced].empty? }
-    list = keys.inject({}) { |list, key| list.merge(key => languages[key]) }
+    list = languages_filtered.reject { |key| languages_filtered[key][:influenced].empty? }
     accumulator = []
     list.each do |key, value|
       accumulator += value[:influenced]
-      value[:influenced] = escape_list(value[:influenced], separator)
+      value[:formatted_influenced] = escape_list(value[:influenced], separator)
     end
     valid_group(list, accumulator)
   end
-  
+
   def render_erb(filename)
     template = ERB.new File.new("genealogy.erb").read, nil, "%"
     File.open("#{filename}.dot", "w+") do |f|
       f.write template.result(binding)
     end
-    
+
     system "dot -Tpng #{filename}.dot -o #{filename}.png"
   end
-  
-  def generate    
+
+  def generate
+    @ancestors = ancestors
+
+    @main_ranks_str = formatted_main_rank_groups(true)
     @valid_influenced = []
-    @valid_versions, @main_ranks_str, @recent_languages_filtered, ancestors_filtered = valid_versions(" -> ")
+    @valid_versions, @recent_languages_filtered, ancestors_filtered = valid_versions(" -> ")
     @ancestors_str = escape_list(ancestors_filtered)
     render_erb('genealogy_versions')
 
+    @main_ranks_str = formatted_main_rank_groups(false)
     @valid_versions = []
-    @valid_influenced, @main_ranks_str, @recent_languages_filtered, ancestors_filtered = valid_influenced
+    @valid_influenced, @recent_languages_filtered, ancestors_filtered = valid_influenced()
     @ancestors_str = escape_list(ancestors_filtered)
     render_erb('genealogy_influenced')
   end
-end
 
+  private
+  def filter_ancestors(filter = nil)
+    return unless filter
+    queue = [language] + languages.keys.select { |key| languages[key][:versions].map(&:first).include?(language) }
+    result = []
+    while !queue.empty?
+      current_language = queue.pop
+      found = languages.keys.select { |key| key != current_language && languages[key][:influenced].include?(current_language) }
+      queue = found + queue
+      result += found
+    end
+    self.languages = result.inject({}) { |list, key| list.merge(key => languages[key]) }
+  end
+end
 Timeline.new.generate
